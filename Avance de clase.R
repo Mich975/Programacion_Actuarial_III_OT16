e4e7e638d478ca8281f16a45e945eb1dfd1cd96a
Michelle Avendaño <si12008@hotmail.com>
2016-08-31 09:43:50 -0500

    Se agrego una primer base de datos
    se descargo de yaho finance la base de precios de S&P y se utilizó para
    ejemplificar el uso de read.table y rade.csv

--- Avance de clase.R
+++ Avance de clase.R
@@ -99,3 +99,88 @@ m2
 
 rbind(m1,y)
 cbind(m2,y)
+
+#Factores 
+x <- factor(c("si","no","si","si","no","si","no"))
+x
+table(x)
+
+unclass(x)
+
+x <- factor(c("si","no","si","si","no","si","no"),levesl = c("si","no"))
+unclass(x)
+
+
+x <- factor(c("azul","azul","rojo","azul","amarillo","verde","azul"))
+x
+table(x)
+
+#Valores Faltantes
+x <- c(1,2,NaN,10,3)
+is.na(x)
+is.nan(x)
+
+
+#Data Frames
+x <- data.frame(Erick = 1:4, Lori = c(T,T,F,F))
+row.names(x) <- c("primero","segundo","tercero","cuarto")
+x
+nrow(x)
+ncol(x)
+attributes(x)
+names(x) <- c("Yarely","Karen")
+x
+
+#Los nombres no son exclusivos de los data frames
+
+x <- 1:3
+names(x) #NULL
+names(x) <- c("Hugo","Paco","Luis")
+x
+
+
+x <- list(a = 1:10, b=100:91,c=51:60)
+x
+names(x) <- c("SeQ1","SeQ2","SeQ3")
+x
+
+m <- matrix(1:4,2,2)
+m
+attributes(m)
+dimnames(m) <- list(c("fil1","fil2"), c("col1","col2")) #nombre en columnas y filas
+m
+
+#Tabla tipo excel
+m <- matrix(nrow=5,ncol=6)
+dimnames(m) <- list(c("1","2","3","4","5"), c("A","B","C","D","E","F"))
+m
+
+#Lectura de Datos
+getwd()
+setwd("~/GitHub/Programacion_Actuarial_III_OT16")
+data <- read.csv("Datos_S&P.Csv")
+data
+data <- read.table("Datos_S&P.csv",T,",")
+data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
